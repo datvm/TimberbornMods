49b491d6ae1bf2d19bc7fcf8deaf3a8ac3cfbf5a
Luke Vo <lukevo93@outlook.com>
2024-12-27 14:03:22 +0700

    Added Instant Build.

---
+++ InstantBuild/ConstructionSiteBuildTimeCalculatorPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Timberborn.ConstructionSites;
+
+namespace InstantBuild;
+
+[HarmonyPatch(typeof(ConstructionSiteBuildTimeCalculator), nameof(ConstructionSiteBuildTimeCalculator.GetConstructionTimeInHours))]
+public static class ConstructionSiteBuildTimeCalculatorPatch
+{
+
+    public static bool Prefix(ref float __result)
+    {
+        __result = .01f;
+        return false;
+    }
+
+}
---
+++ InstantBuild/ModStarter.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Timberborn.ModManagerScene;
+
+namespace InstantBuild;
+
+public class ModStarter : IModStarter
+{
+
+    public void StartMod(IModEnvironment modEnvironment)
+    {
+        new Harmony(nameof(InstantBuild)).PatchAll();
+    }
+
+}
