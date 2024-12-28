ab0d72187d7cf9c84d0f9eb64fdbadd68702d72a
Luke Vo <lukevo93@outlook.com>
2024-12-28 10:14:56 +0700

    Added AlwaysFlexibleStart.

---
+++ AlwaysFlexibleStart/GameWonderCompletionServicePatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Timberborn.GameWonderCompletion;
+
+namespace AlwaysFlexibleStart;
+
+[HarmonyPatch(typeof(GameWonderCompletionService), nameof(GameWonderCompletionService.IsWonderCompletedWithAnyFaction))]
+public static class GameWonderCompletionServicePatch
+{
+
+    public static bool Prefix(ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
+
+}
---
+++ AlwaysFlexibleStart/ModStarter.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using Timberborn.ModManagerScene;
+
+namespace AlwaysFlexibleStart;
+public class ModStarter : IModStarter
+{
+
+    public void StartMod(IModEnvironment modEnvironment)
+    {
+        new Harmony(nameof(AlwaysFlexibleStart)).PatchAll();
+    }
+
+}
