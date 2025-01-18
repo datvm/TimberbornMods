0694257a2788cbad798fd036c5badd730fb87e69
Luke Vo <lukevo93@outlook.com>
2025-01-18 13:03:01 +0700

    Added Unlimited District Range.

---
+++ UnlimitedDistrictRange/ModStarter.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using Timberborn.ModManagerScene;
+
+namespace UnlimitedDistrictRange;
+internal class ModStarter : IModStarter
+{
+
+    public void StartMod(IModEnvironment modEnvironment)
+    {
+        new Harmony(nameof(UnlimitedDistrictRange)).PatchAll();
+    }
+
+}
---
+++ UnlimitedDistrictRange/NavigationDistancePatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Timberborn.Navigation;
+
+namespace UnlimitedDistrictRange;
+
+[HarmonyPatch(typeof(NavigationDistance), nameof(NavigationDistance.DistrictTerrain), MethodType.Getter)]
+public static class NavigationDistancePatch
+{
+
+    public static bool Prefix(ref int __result)
+    {
+        __result = 500;
+        return false;
+    }
+
+}
