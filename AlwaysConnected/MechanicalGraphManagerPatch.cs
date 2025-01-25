b72059d637ccb48f363ba31910d09e3383110e6d
Luke Vo <lukevo93@outlook.com>
2025-01-26 00:08:30 +0700

    Added Always Connected.

---
+++ AlwaysConnected/MechanicalGraphManagerPatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Timberborn.MechanicalSystem;
+
+namespace AlwaysConnected;
+
+[HarmonyPatch(typeof(MechanicalGraphManager))]
+public static class MechanicalGraphManagerPatch
+{
+    static readonly FieldInfo mechanicalGraphRegistryField = typeof(MechanicalGraphFactory).GetField("_mechanicalGraphRegistry", BindingFlags.NonPublic | BindingFlags.Instance);
+
+    [HarmonyPatch(nameof(MechanicalGraphManager.AddNode)), HarmonyPostfix]
+    public static void PostfixAddNode(MechanicalGraphFactory ____mechanicalGraphFactory)
+    {
+        Patch(____mechanicalGraphFactory);
+    }
+
+    [HarmonyPatch(nameof(MechanicalGraphManager.RemoveNode)), HarmonyPostfix]
+    public static void PostfixRemoveNode(MechanicalGraphFactory ____mechanicalGraphFactory)
+    {
+        Patch(____mechanicalGraphFactory);
+    }
+
+    static void Patch(MechanicalGraphFactory ____mechanicalGraphFactory)
+    {
+        var registry = (MechanicalGraphRegistry)mechanicalGraphRegistryField.GetValue(____mechanicalGraphFactory);
+
+        ____mechanicalGraphFactory.Join([.. registry.MechanicalGraphs]);
+    }
+
+}
---
+++ AlwaysConnected/ModStarter.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using Timberborn.ModManagerScene;
+
+namespace AlwaysConnected;
+public class ModStarter : IModStarter
+{
+
+    public void StartMod(IModEnvironment modEnvironment)
+    {
+        new Harmony(nameof(AlwaysConnected)).PatchAll();
+    }
+
+}
