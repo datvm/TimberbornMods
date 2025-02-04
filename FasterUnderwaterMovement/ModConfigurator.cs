f460f409689776401818b8945fa9019a510189a6
Luke Vo <lukevo93@outlook.com>
2025-02-05 02:25:45 +0700

    Added Faster Underwater Movement

---
+++ FasterUnderwaterMovement/ModConfigurator.cs
@@ -0,0 +1,11 @@
+﻿namespace FasterUnderwaterMovement;
+
+[Context("MainMenu")]
+[Context("Game")]
+public class ModConfigurator : IConfigurator
+{
+    public void Configure(IContainerDefinition containerDefinition)
+    {
+        containerDefinition.Bind<ModSettings>().AsSingleton();
+    }
+}
---
+++ FasterUnderwaterMovement/ModSettings.cs
@@ -0,0 +1,28 @@
+﻿global using Timberborn.WalkingSystem;
+
+namespace FasterUnderwaterMovement;
+
+public class ModSettings(ISettings settings, ModSettingsOwnerRegistry modSettingsOwnerRegistry, ModRepository modRepository) : ModSettingsOwner(settings, modSettingsOwnerRegistry, modRepository)
+{
+    static readonly FieldInfo SwimmingPenalty = typeof(WalkerSpeedManager).GetField("SwimmingPenalty", BindingFlags.NonPublic | BindingFlags.Static);
+
+    protected override string ModId => nameof(FasterUnderwaterMovement);
+    public override ModSettingsContext ChangeableOn => ModSettingsContext.All;
+
+    protected override void OnAfterLoad()
+    {
+        ModSetting<float> bonus = new(.5f, ModSettingDescriptor
+            .CreateLocalized("LV.FUM.SwimmingBonus")
+            .SetLocalizedTooltip("LV.FUM.SwimmingBonusDesc"));
+
+        AddCustomModSetting(bonus, nameof(bonus));
+
+        bonus.ValueChanged += (_, value) => SetValue(value);
+        SetValue(bonus.Value);
+    }
+
+    void SetValue(float bonus)
+    {
+        SwimmingPenalty.SetValue(null, -bonus);
+    }
+}
