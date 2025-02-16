09fe82d7210c946c8d091a2846fc758c1801c4de
Luke Vo <lukevo93@outlook.com>
2025-02-17 01:38:31 +0700

    Added MaterialPlzNoCrashes

---
+++ MaterialPlzNoCrashes/Patch.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection.Emit;
+using Timberborn.TimbermeshMaterials;
+
+namespace MaterialPlzNoCrashes;
+
+[HarmonyPatch]
+public class Patch
+{
+
+    static readonly MethodInfo PrintWarningInsteadMethod = typeof(Patch).Method(nameof(PrintWarningInstead));
+
+    static void PrintWarningInstead(object ex)
+    {
+        Debug.LogWarning("Duplicate materials detected: " + ex.ToString());
+    }
+
+    [HarmonyTranspiler, HarmonyPatch(typeof(MaterialRepository), nameof(MaterialRepository.LoadMaterials))]
+    public static IEnumerable<CodeInstruction> PatchLoadDuplicateMaterials(IEnumerable<CodeInstruction> instructions)
+    {
+        foreach (var ins in instructions)
+        {
+            if (ins.opcode == OpCodes.Throw)
+            {
+                // Call Debug.LogWarning instead
+                yield return new CodeInstruction(OpCodes.Call, PrintWarningInsteadMethod);
+            }
+            else
+            {
+                yield return ins;
+            }
+        }
+    }
+
+}
+
+public class ModStarter : IModStarter
+{
+
+    public void StartMod(IModEnvironment modEnvironment)
+    {
+        new Harmony(nameof(MaterialPlzNoCrashes)).PatchAll();
+    }
+
+}
+
