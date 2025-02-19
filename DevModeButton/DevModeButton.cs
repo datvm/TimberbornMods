cf0230915e7bbdc8e0cafd9dd595ec6fbc059e99
Luke Vo <lukevo93@outlook.com>
2025-02-20 05:48:54 +0700

    Added DevMode button as a separate mod.

---
+++ DevModeButton/DevModeButton.cs
@@ -0,0 +1,31 @@
+﻿using Timberborn.AssetSystem;
+using Timberborn.Debugging;
+
+
+namespace DevModeButton;
+public class DevModeButton : GrouplessBottomBarButton
+{
+    readonly DevModeManager devMode;
+
+    public DevModeButton(VisualElementLoader veLoader, IAssetLoader assetLoader, DevModeManager devMode) : base(veLoader, assetLoader)
+    {
+        SpritePath = "Sprites/timberui-dev";
+        Click = ToggleDevMode;
+        BottomText = "Dev Mode";
+
+        this.devMode = devMode;
+    }
+
+    void ToggleDevMode()
+    {
+        if (devMode.Enabled)
+        {
+            devMode.Disable();
+        }
+        else
+        {
+            devMode.Enable();
+        }
+    }
+
+}
---
+++ DevModeButton/ModConfigs.cs
@@ -0,0 +1,15 @@
+﻿global using Timberborn.BottomBarSystem;
+
+namespace DevModeButton;
+
+[Context("Game")]
+public class GameConfigs : Configurator
+{
+
+    public override void Configure()
+    {
+        Bind<DevModeButton>().AsSingleton();
+        MultiBind<BottomBarModule>().ToProvider<BottomBarModuleProvider<DevModeButton>>().AsSingleton();
+    }
+
+}
