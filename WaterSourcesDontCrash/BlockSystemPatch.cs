ed95310d76e95cc47804454012d2595a33a17b9f
Luke Vo <lukevo93@outlook.com>
2025-03-04 14:52:55 +0700

    No more Ground-only v7.1.1: fix too many crashes.

--- WaterSourcesDontCrash/BlockSystemPatch.cs
+++ WaterSourcesDontCrash/BlockSystemPatch.cs
@@ -29,6 +29,12 @@ public static class BlockObjectPatch
         if (blocks.Length == 0) { return true; }
 
         var topZ = blocks.Max(block => block.Coordinates.z);
+        var bottomZ = blocks.Min(block => block.Coordinates.z);
+
+        // Limit to 3x3x2 or 5x5x2 blocks only
+        if (topZ - bottomZ != 1
+            || (blocks.Length != 18 && blocks.Length != 50)) { return true; }
+
         var topBlocks = blocks.Where(block => block.Coordinates.z == topZ).ToImmutableArray();
 
         __result = topBlocks.All((Block block) => __instance.BlockValid(block, almost: false, ignoreUnfinishedStackable: false));
