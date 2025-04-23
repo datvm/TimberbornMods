b536cf1b7323520f564c1b13a1be011897523ffc
Luke Vo <lukevo93@outlook.com>
2025-04-23 16:13:16 +0700

    Added ScrollableEntityPanel

---
+++ ScrollableEntityPanel/ScollAdder.cs
@@ -0,0 +1,52 @@
+﻿namespace ScrollableEntityPanel;
+public class ScollAdder(IEntityPanel entityPanel, VisualElementInitializer veInit) : ILoadableSingleton
+{
+    readonly EntityPanel entityPanel = entityPanel as EntityPanel
+        ?? throw new InvalidDataException($"{nameof(IEntityPanel)} is not {nameof(EntityPanel)}");
+
+    public void Load()
+    {
+        var root = entityPanel._root;
+        root.style.maxHeight = new Length(80, LengthUnit.Percent);
+
+        var stockpileInventory = root.Q("StockpileInventoryFragmentWrapper");
+        var fragments = root.Q("Fragments");
+        var parent = fragments.parent;
+
+        var scrollContainer = new VisualElement();
+
+        var scroll = new ScrollView()
+        {
+            name = "FragmentScroll"
+        };
+        scroll.classList.Add("game-scroll-view");
+        veInit.InitializeVisualElement(scroll);
+
+
+        var index = -1;
+        var curr = 0;
+        foreach (var child in parent.Children())
+        {
+            if (child == fragments)
+            {
+                index = curr;
+            }
+            else
+            {
+                child.style.flexShrink = 0;
+            }
+
+            curr++;
+        }
+
+        scroll.Add(fragments);
+        parent.Insert(index, scrollContainer);
+
+        // Move the Good selection panel out of the scroll to prevent issue
+        stockpileInventory.style.flexShrink = 0;
+        scrollContainer.Add(stockpileInventory);
+
+        scrollContainer.Add(scroll);
+
+    }
+}
