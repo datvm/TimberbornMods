c846c492699d1a28c335d92ff0a0740fc541fce5
Luke Vo <lukevo93@outlook.com>
2025-04-26 22:53:55 +0700

    CW 7.0.2

--- ConfigurableWorkplace/Patches/WorkerPatches.cs
+++ ConfigurableWorkplace/Patches/WorkerPatches.cs
@@ -1,45 +1,124 @@
-﻿global using Timberborn.WorkSystem;
+﻿global using System.Runtime.CompilerServices;
 global using Timberborn.EnterableSystem;
+global using Timberborn.PrefabGroupSystem;
+global using Timberborn.SlotSystem;
+global using Timberborn.WorkSystem;
+global using Timberborn.MechanicalSystem;
 
 namespace ConfigurableWorkplace.Patches;
+
+public record OriginalWorkplaceValues(int MaxWorker, bool DisallowOthers)
+{
+    public int PowerOutput { get; set; }
+}
 
 [HarmonyPatch]
 public static class WorkerPatches
 {
+    static readonly ConditionalWeakTable<GameObject, OriginalWorkplaceValues> ConfiguredPrefabs = [];
 
-    [HarmonyPostfix, HarmonyPatch(typeof(WorkplaceSpec), nameof(WorkplaceSpec.MaxWorkers), MethodType.Getter)]
-    public static void MultiplyMaxWorkers(ref int __result)
+    [HarmonyPostfix, HarmonyPatch(typeof(PrefabGroupService), nameof(PrefabGroupService.Load))]
+    public static void ModifyWorkplaces(PrefabGroupService __instance)
     {
-        var mul = MSettings.MaxWorkerMul;
+        if (MSettings.MaxWorkerMul == 1 && !MSettings.BotEverywhere && ConfiguredPrefabs.Count() == 0) { return; }
 
-        if (mul != 1f && __result > 0)
+        foreach (var prefab in __instance.AllPrefabs)
         {
-            __result = Math.Max(1, Mathf.FloorToInt(__result * mul));
+            var workplace = prefab.GetComponent<WorkplaceSpec>();
+            if (!workplace) { continue; }
+
+            if (!ConfiguredPrefabs.TryGetValue(prefab, out var originalValues))
+            {
+                originalValues = new(workplace.MaxWorkers, workplace.DisallowOtherWorkerTypes);
+                ConfiguredPrefabs.Add(prefab, originalValues);
+            }
+
+            if (originalValues.MaxWorker > 0)
+            {
+                var newMaxWorker = Math.Max(1, Mathf.FloorToInt(originalValues.MaxWorker * MSettings.MaxWorkerMul));
+                if (newMaxWorker == workplace.MaxWorkers) { continue; }
+
+                ModifyMaxWorker(workplace, newMaxWorker, originalValues);
+                ModifyMaxWorkerPower(workplace, newMaxWorker, originalValues);
+            }
+
+            workplace._disallowOtherWorkerTypes = !MSettings.BotEverywhere && originalValues.DisallowOthers;
         }
     }
 
-    [HarmonyPrefix, HarmonyPatch(typeof(WorkplaceSpec), nameof(WorkplaceSpec.DisallowOtherWorkerTypes), MethodType.Getter)]
-    public static bool AllowBots(ref bool __result)
+    static void ModifyMaxWorker(WorkplaceSpec wp, int maxWorker, OriginalWorkplaceValues originalValues)
     {
-        if (MSettings.BotEverywhere)
-        {
-            __result = false;
-            return false;
-        }
+        wp._maxWorkers = maxWorker;
 
-        return true;
+        var enterableSpec = wp.GetComponentFast<EnterableSpec>();
+        if (!enterableSpec.LimitedCapacityFinished || enterableSpec.CapacityFinished == maxWorker) { return; }
+        enterableSpec._capacityFinished = maxWorker;
+
+        AddSlotsIfNeeded<PatrollingSlotInitializerSpec, PatrollingSlotSpec>(
+            wp,
+            originalValues.MaxWorker,
+            comp => comp._patrollingSlots,
+            maxWorker);
+
+        AddSlotsIfNeeded<TransformSlotInitializerSpec, TransformSlotSpec>(
+            wp,
+            originalValues.MaxWorker,
+            comp => comp._slots,
+            maxWorker);
     }
 
-    [HarmonyPrefix, HarmonyPatch(typeof(Workplace), nameof(Workplace.ValidateEnterable))]
-    public static void AllowEnterable(Workplace __instance)
+    static void ModifyMaxWorkerPower(WorkplaceSpec wp, int maxWorker, OriginalWorkplaceValues originalValues)
     {
-        if (MSettings.MaxWorkerMul != 1)
-        {
-            var enterable = __instance.GetComponentFast<EnterableSpec>();
-            if (!enterable || !enterable.LimitedCapacityFinished) { return; }
+        var spec = wp.GetComponentFast<MechanicalNodeSpec>();
+        if (!spec) { return; }
 
-            enterable._capacityFinished = __instance._workplaceSpec.MaxWorkers;
+        if (originalValues.PowerOutput == 0)
+        {
+            originalValues.PowerOutput = spec._powerOutput;
         }
+
+        var ratio = maxWorker / (float)originalValues.MaxWorker;
+        var newPower = Mathf.RoundToInt(originalValues.PowerOutput * ratio);
+        spec._powerOutput = newPower;
+    }
+
+    static void AddSlotsIfNeeded<TComp, TSpec>(WorkplaceSpec wp, int originalSlotCount, Func<TComp, List<TSpec>> listFunc, int neededSlots)
+        where TComp : BaseComponent
+        where TSpec : notnull, new()
+    {
+        var slotSpecs = wp.GetComponentFast<TComp>();
+        if (!slotSpecs) { return; }
+
+        var list = listFunc(slotSpecs);
+        if (list.Count >= neededSlots) { return; }
+
+        var addNeeded = neededSlots - list.Count;
+        for (int i = 0; i < addNeeded; i++)
+        {
+            var newSlot = CopySerializable(list[i % originalSlotCount]);
+            list.Add(newSlot);
+        }
+    }
+
+    static readonly Dictionary<Type, ImmutableArray<FieldInfo>> fieldsCache = [];
+    static T CopySerializable<T>(T src) where T : notnull, new()
+    {
+        var type = src.GetType(); // Don't use typeof<T>
+
+        if (!fieldsCache.TryGetValue(type, out var fields))
+        {
+            fields = [.. type.GetFields(AccessTools.all)
+                .Where(q =>!q.IsStatic && q.GetCustomAttribute<SerializeField>() is not null)];
+        }
+
+        var result = new T();
+
+        foreach (var field in fields)
+        {
+            field.SetValue(result, field.GetValue(src));
+        }
+
+        return result;
     }
 
 }
