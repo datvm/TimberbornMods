3ab321eb271c8b99552a5a8ac0094d3838b23ff9
Luke Vo <lukevo93@outlook.com>
2025-05-03 06:05:30 -0500

    Added Configurable Lifetime.

---
+++ ConfigurableLifetime/MSettings.cs
@@ -0,0 +1,34 @@
+﻿namespace ConfigurableLifetime;
+
+public class MSettings(
+  ISettings settings,
+  ModSettingsOwnerRegistry modSettingsOwnerRegistry,
+  ModRepository modRepository) : ModSettingsOwner(settings, modSettingsOwnerRegistry, modRepository), IUnloadableSingleton
+{
+
+    public static MSettings? Instance { get; private set; }
+
+    public override string ModId { get; } = nameof(ConfigurableLifetime);
+    public override ModSettingsContext ChangeableOn { get; } = ModSettingsContext.All;
+
+    public ModSetting<float> BeaverLifeMul { get; } = new(1f, ModSettingDescriptor
+        .CreateLocalized("LV.CLt.BeaverLifeMul")
+        .SetLocalizedTooltip("LV.CLt.BeaverLifeMulDesc"));
+    public ModSetting<float> ChildhoodDaysMul { get; } = new(1f, ModSettingDescriptor
+        .CreateLocalized("LV.CLt.ChildhoodDaysMul")
+        .SetLocalizedTooltip("LV.CLt.ChildhoodDaysMulDesc"));
+    public ModSetting<float> BotLifeMul { get; } = new(1.0f, ModSettingDescriptor
+        .CreateLocalized("LV.CLt.BotLifeMul")
+        .SetLocalizedTooltip("LV.CLt.BotLifeMulDesc"));
+
+    public override void OnAfterLoad()
+    {
+        base.OnAfterLoad();
+        Instance = this;
+    }
+
+    public void Unload()
+    {
+        Instance = null;
+    }
+}
---
+++ ConfigurableLifetime/Patches/LifetimePatches.cs
@@ -0,0 +1,65 @@
+﻿namespace ConfigurableLifetime.Patches;
+
+[HarmonyPatch]
+public static class LifetimePatches
+{
+    static readonly ConditionalWeakTable<LifeProgressor, object> characterCache = [];
+
+    [HarmonyPrefix, HarmonyPatch(typeof(LifeProgressor), nameof(LifeProgressor.IncreaseLifeProgress))]
+    public static bool StopLifeProgress(LifeProgressor __instance)
+    {
+        var mul = MSettings.Instance?.BeaverLifeMul.Value;
+        if (mul is null or 1f || IsChild(__instance))
+        {
+            return true;
+        }
+
+        __instance.LifeProgress +=
+            __instance._lifeProgressIncreasePerTick
+            * mul.Value
+            / __instance._bonusManager.Multiplier(LifeProgressor.LifeExpectancyBonusId);
+
+        return false;
+    }
+
+    [HarmonyPrefix, HarmonyPatch(typeof(Deteriorable), nameof(Deteriorable.Tick))]
+    public static void MultiplyDeteriorableLifeProgress(Deteriorable __instance)
+    {
+        var mul = MSettings.Instance?.BotLifeMul.Value;
+        if (__instance._currentDeterioration <= 0f || mul is null or 1f) { return; }
+
+        // Give the difference between this and the supposed _currentDeterioration -= _fixedDeltaTimeInDays;
+        var willBeValue = __instance._currentDeterioration - __instance._fixedDeltaTimeInDays;
+        var shouldBeValue = __instance._currentDeterioration - __instance._fixedDeltaTimeInDays * mul.Value;
+        var diff = willBeValue - shouldBeValue;
+        __instance._currentDeterioration -= diff;
+    }
+
+    [HarmonyPostfix, HarmonyPatch(typeof(Child), nameof(Child.GrowthProgressPerTick))]
+    public static void MultiplyChildGrowth(ref float __result)
+    {
+        var mul = MSettings.Instance?.ChildhoodDaysMul.Value;
+        if (mul is null or 1f) { return; }
+
+        __result *= mul.Value;
+    }
+
+    static bool IsChild(LifeProgressor lifeProgressor)
+    {
+        if (!characterCache.TryGetValue(lifeProgressor, out var value))
+        {
+            var child = lifeProgressor.GetComponentFast<Child>();
+            characterCache.Add(lifeProgressor, (bool?)child);
+        }
+
+        return (bool?)value == true;
+    }
+
+}
+
+public enum CharacterType
+{
+    Adult,
+    Child,
+    Bot
+}
