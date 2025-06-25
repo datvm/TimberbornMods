fdeecdcd887a005af2bd64427d11a832edf13dda
Luke Vo <lukevo93@outlook.com>
2025-06-24 22:16:00 -0500

    Added Useless Machine.

---
+++ UselessMachine/MainMenuService.cs
@@ -0,0 +1,19 @@
+﻿namespace UselessMachine;
+
+public class MainMenuService(ModRepository mods) : ILoadableSingleton
+{
+    public void Load()
+    {
+        ModPlayerPrefsHelper.ToggleMod(false, mods.EnabledMods.First(q => q.Manifest.Id == nameof(UselessMachine)));
+        GameQuitter.Quit();
+    }
+}
+
+[Context("MainMenu")]
+public class ModMenuConfig : Configurator
+{
+    public override void Configure()
+    {
+        Bind<MainMenuService>().AsSingleton();
+    }
+}
