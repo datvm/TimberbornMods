f3b8af35d7e35c805899075cfbfe91deeda2203b
Luke Vo <lukevo93@outlook.com>
2025-07-31 00:39:27 -0500

    Timber Dump 7.1.2

---
+++ TimberDump/Services/Dumpers/LocDumper.cs
@@ -0,0 +1,27 @@
+﻿namespace TimberDump.Services.Dumpers;
+
+public class LocDumper(ILoc t) : IDumper
+{
+    readonly Loc t = (Loc)t;
+
+    public int Order { get; }
+    public string? Folder { get; }
+
+    public void Dump(string folder)
+    {
+        var filePath = Path.Combine(folder, "localizations.csv");
+
+        using var writer = new StreamWriter(filePath);
+        writer.WriteLine("ID,Text,Comment");
+
+        foreach (var (key, value) in t._localization)
+        {
+            writer.WriteLine(
+                $"{key},{EscapeCsvValue(value)},"
+            );
+        }
+    }
+
+    static string EscapeCsvValue(string value) => $"\"{value.Replace("\"", "\"\"")}\"";
+
+}
