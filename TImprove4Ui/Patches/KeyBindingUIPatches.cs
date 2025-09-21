6bf302d546ed686605afae919588ac819711e084
Luke Vo <lukevo93@outlook.com>
2025-09-21 17:24:18 -0500

    T4UI 7.10.0

---
+++ TImprove4Ui/Patches/KeyBindingUIPatches.cs
@@ -0,0 +1,14 @@
+﻿namespace TImprove4Ui.Patches;
+
+[HarmonyPatch]
+public static class KeyBindingUIPatches
+{
+
+    [HarmonyPrefix, HarmonyPatch(typeof(KeyBindingGroupSpec), nameof(KeyBindingGroupSpec.IsHiddenGroup), MethodType.Getter)]
+    public static bool PatchHiddenGroup(ref bool __result)
+    {
+        __result = false;
+        return false;
+    }
+
+}
