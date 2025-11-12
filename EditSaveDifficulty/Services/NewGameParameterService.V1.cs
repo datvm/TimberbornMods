2e3e0f7ce1cf4f73364ed1a723a1b8971d7cc3de
Luke Vo <lukevo93@outlook.com>
2025-11-12 13:43:30 +0700

    ESD 10.0.0

---
+++ EditSaveDifficulty/Services/NewGameParameterService.V1.cs
@@ -0,0 +1,68 @@
+﻿namespace EditSaveDifficulty.Services;
+
+public class NewGameParameterService(
+    NeedModificationService needMods,
+    TemperateWeatherDurationService temperWeather,
+    DroughtWeather droughtWeather,
+    BadtideWeather badtideWeather,
+    EffectProbabilityService effectProb,
+    GoodRecoveryRateService goodRec
+)
+{
+
+    public NewGameModeSpec GatherCurrentParameters() => new()
+    {
+        StartingAdults = 0,
+        AdultAgeProgress = new(),
+        StartingChildren = 0,
+        ChildAgeProgress = new(),
+        FoodConsumption = needMods._foodConsumption,
+        WaterConsumption = needMods._waterConsumption,
+        StartingFood = 0,
+        StartingWater = 0,
+        TemperateWeatherDuration = new MinMaxSpec<int>
+        {
+            Min = temperWeather._minTemperateWeatherDuration,
+            Max = temperWeather._maxTemperateWeatherDuration
+        },
+        DroughtDuration = new MinMaxSpec<int>
+        {
+            Min = droughtWeather._minDroughtDuration,
+            Max = droughtWeather._maxDroughtDuration
+        },
+        DroughtDurationHandicapMultiplier = droughtWeather._handicapMultiplier,
+        DroughtDurationHandicapCycles = droughtWeather._handicapCycles,
+        CyclesBeforeRandomizingBadtide = badtideWeather._cyclesBeforeRandomizingBadtideWeather,
+        ChanceForBadtide = badtideWeather.ChanceForBadtide,
+        BadtideDuration = new MinMaxSpec<int>
+        {
+            Min = badtideWeather._minDuration,
+            Max = badtideWeather._maxDuration
+        },
+        BadtideDurationHandicapMultiplier = badtideWeather._handicapMultiplier,
+        BadtideDurationHandicapCycles = badtideWeather._handicapCycles,
+        InjuryChance = effectProb._injuryChanceModifier,
+        DemolishableRecoveryRate = goodRec.DemolishableRecoveryRate
+    };
+
+    public void SetNewParameters(NewGameModeSpec parameters)
+    {
+        needMods._foodConsumption = parameters.FoodConsumption;
+        needMods._waterConsumption = parameters.WaterConsumption;
+        temperWeather._minTemperateWeatherDuration = parameters.TemperateWeatherDuration.Min;
+        temperWeather._maxTemperateWeatherDuration = parameters.TemperateWeatherDuration.Max;
+        droughtWeather._minDroughtDuration = parameters.DroughtDuration.Min;
+        droughtWeather._maxDroughtDuration = parameters.DroughtDuration.Max;
+        droughtWeather._handicapMultiplier = parameters.DroughtDurationHandicapMultiplier;
+        droughtWeather._handicapCycles = parameters.DroughtDurationHandicapCycles;
+        badtideWeather._cyclesBeforeRandomizingBadtideWeather = parameters.CyclesBeforeRandomizingBadtide;
+        badtideWeather.ChanceForBadtide = parameters.ChanceForBadtide;
+        badtideWeather._minDuration = parameters.BadtideDuration.Min;
+        badtideWeather._maxDuration = parameters.BadtideDuration.Max;
+        badtideWeather._handicapMultiplier = parameters.BadtideDurationHandicapMultiplier;
+        badtideWeather._handicapCycles = parameters.BadtideDurationHandicapCycles;
+        effectProb._injuryChanceModifier = parameters.InjuryChance;
+        goodRec.DemolishableRecoveryRate = parameters.DemolishableRecoveryRate;
+    }
+
+}
---
+++ EditSaveDifficulty/UI/EditDifficultyDialog.V1.cs
@@ -0,0 +1,67 @@
+﻿namespace EditSaveDifficulty.UI;
+
+public class EditDifficultyDialog(
+    VisualElementLoader veLoader,
+    PanelStack panelStack,
+    ILoc t,
+    CustomNewGameModeController customNewGameModeController,
+    ISpecService specService
+) : DialogBoxElement
+{
+    static readonly ImmutableArray<string> InvisibleNames = [
+        "Modes", "BackButton", "NextButton", 
+        "StartingAdultsWrapper", "StartingChildrenWrapper",
+        "StartingFoodWrapper", "StartingWaterWrapper"
+    ];
+
+    public NewGameModeSpec NewGameMode { get; private set; } = null!;
+
+    public void SetContent(FactionSpec factionSpec, string mapName, NewGameModeSpec current)
+    {
+        SetTitle("LV.ESD.ChangeDifficulty".T(t));
+        AddCloseButton();
+
+        var warningContent = "LV.ESD.SaveWarning".T(t).Bold().Color(TimberbornTextColor.Red);
+        Content.AddGameLabel(warningContent, centered: true);
+
+        var newGamePanel = new NewGameModePanel(veLoader, null, panelStack, t, customNewGameModeController, specService);
+        newGamePanel.Load();
+
+        newGamePanel.SelectFactionAndMap(
+            factionSpec,
+            // only map name is accessed:
+            new(default, mapName, null, null, false, false, false, false, null));
+
+        newGamePanel._predefinedNewGameMode = NewGameMode = current;
+        newGamePanel.OnCustomizeButtonClicked();
+
+        var root = newGamePanel.GetPanel();
+        foreach (var name in InvisibleNames)
+        {
+            root.Q(name)?.SetDisplay(false);
+        }
+
+        Content.Add(root.parent);
+
+        Content.AddGameLabel(warningContent, centered: true);
+        Content.AddMenuButton("LV.ESD.ChangeDifficulty".T(t), 
+            onClick: OnConfirm,
+            size: UiBuilder.GameButtonSize.Large,
+            stretched: true);
+
+        style.maxWidth = new Length(100, LengthUnit.Percent);
+        style.maxHeight = new Length(100, LengthUnit.Percent);
+    }
+
+    void OnConfirm()
+    {
+        if (!customNewGameModeController.TryGetValidatedNewGameMode(out var newGameMode))
+        {
+            return;
+        }
+
+        NewGameMode = newGameMode;
+        OnUIConfirmed();
+    }
+
+}
