86a40165da97ff1785334a355f1400fc59019658
Luke Vo <lukevo93@outlook.com>
2025-11-15 23:36:57 +0700

    CBW 10.0.1

--- ConfigurableBeaverWalk/Patches/WorkingSpeedPatches.cs
+++ ConfigurableBeaverWalk/Patches/WorkingSpeedPatches.cs
@@ -1,4 +1,6 @@
-﻿namespace ConfigurableBeaverWalk.Patches;
+﻿using System.Reflection.Emit;
+
+namespace ConfigurableBeaverWalk.Patches;
 
 [HarmonyPatch]
 public static class WorkingSpeedPatches
@@ -20,4 +22,38 @@ public static class WorkingSpeedPatches
         }
     }
 
+    [HarmonyTranspiler, HarmonyPatch(typeof(WorkAtReservableExecutor), nameof(WorkAtReservableExecutor.Tick))]
+    public static IEnumerable<CodeInstruction> Test(IEnumerable<CodeInstruction> instructions)
+    {
+        var expectingMethod = typeof(IDayNightCycle).PropertyGetter(nameof(IDayNightCycle.FixedDeltaTimeInHours));
+
+        foreach (var ins in instructions)
+        {
+            yield return ins;
+
+            if (ins.Calls(expectingMethod))
+            {
+                // Multiply it with worker speed
+                yield return new CodeInstruction(OpCodes.Ldarg_0); // Load 'this' (WorkAtReservableExecutor instance)
+                yield return CodeInstruction.Call(() => GetMultipliedDeltaTime);
+            }
+        }
+    }
+
+    static float? MinimumAmount;
+    [HarmonyPostfix, HarmonyPatch(typeof(WorkAtReservableExecutor), nameof(WorkAtReservableExecutor.CalculateFinishTimestamp))]
+    public static void EnsureOneTick(ref float __result, WorkAtReservableExecutor __instance)
+    {
+        var dnc = __instance._dayNightCycle;
+
+        MinimumAmount ??= dnc.FixedDeltaTimeInHours * 2 / 24f;
+        var min = dnc.PartialDayNumber + MinimumAmount.Value;
+        if (__result < min)
+        {
+            __result = min;
+        }
+    }
+
+    static float GetMultipliedDeltaTime(float fixedDeltaTimeHour, WorkAtReservableExecutor instance)
+        => instance._worker.WorkingSpeedMultiplier * fixedDeltaTimeHour;
 }
