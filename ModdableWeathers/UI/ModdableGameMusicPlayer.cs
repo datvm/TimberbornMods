e04382869adad71f30346eca7f1333e04903734c
Luke Vo <lukevo93@outlook.com>
2025-12-28 16:14:12 +0700

    MW 10.0.3

---
+++ ModdableWeathers/UI/ModdableGameMusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿namespace ModdableWeathers.UI;
+
+[ReplaceSingleton]
+[BypassMethods([
+    nameof(OnHazardousWeatherStarted),
+    nameof(OnHazardousWeatherEnded),
+])]
+public class ModdableGameMusicPlayer(ISoundSystem soundSystem, IRandomNumberGenerator randomNumberGenerator, WeatherService weatherService, EventBus eventBus, RootObjectProvider rootObjectProvider, ISpecService specService) : GameMusicPlayer(soundSystem, randomNumberGenerator, weatherService, eventBus, rootObjectProvider, specService)
+{
+
+    [OnEvent]
+    public void OnWeatherTransitioned(WeatherTransitionedEvent e)
+    {
+        var isHazardous = e.To.Weather.IsHazardous;
+
+        if (isHazardous)
+        {
+            StopStandardMusic();
+            StartDroughtMusic();
+        }
+        else
+        {
+            StopDroughtMusic();
+            StartStandardMusic();
+        }
+    }
+
+}
