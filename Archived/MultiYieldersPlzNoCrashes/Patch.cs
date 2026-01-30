737785989ffa066ec621563094bc36bafc6b60ba
Luke Vo <lukevo93@outlook.com>
2026-01-30 22:00:53 +0700

    Archived MYPNC

---
+++ Archived/MultiYieldersPlzNoCrashes/Patch.cs
@@ -0,0 +1,25 @@
+﻿namespace MultiYieldersPlzNoCrashes;
+
+[HarmonyPatch]
+public static class Patch
+{
+
+    [HarmonyPostfix, HarmonyPatch(typeof(GathererFlag), nameof(GathererFlag.InitializeEntity))]
+    public static void DistinctGatherables(GathererFlag __instance)
+    {
+        __instance.AllowedGatherables = [..__instance.AllowedGatherables
+            .Distinct(GatherableEqualizer.Instance)];
+    }
+
+    class GatherableEqualizer : IEqualityComparer<GatherableSpec>
+    {
+
+        public static readonly GatherableEqualizer Instance = new();
+
+        public bool Equals(GatherableSpec x, GatherableSpec y) => x.Yielder.Yield.Id.Equals(y.Yielder.Yield.Id);
+
+        public int GetHashCode(GatherableSpec obj) => obj.Yielder.Yield.Id.GetHashCode();
+
+    }
+
+}
