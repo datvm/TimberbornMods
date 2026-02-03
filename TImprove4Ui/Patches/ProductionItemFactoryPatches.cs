a00d05cd26f85a3f05ed27da6e674accebd67870
Luke Vo <lukevo93@outlook.com>
2026-02-03 23:05:03 +0700

    T4UI 10.1.2

---
+++ TImprove4Ui/Patches/ProductionItemFactoryPatches.cs
@@ -0,0 +1,18 @@
+﻿namespace TImprove4Ui.Patches;
+
+[HarmonyPatch(typeof(ProductionItemFactory))]
+public static class ProductionItemFactoryPatches
+{
+
+    [HarmonyPostfix, HarmonyPatch(nameof(ProductionItemFactory.CreateInput), [typeof(IEnumerable<VisualElement>)])]
+    public static void WrapInput(VisualElement __result) => WrapIO(__result);
+
+    static void WrapIO(VisualElement e)
+    {
+        var el = e.Q(className: "production-item__items--input");
+        if (el is null) { return; }
+
+        el.style.flexWrap = Wrap.Wrap;
+    }
+
+}
