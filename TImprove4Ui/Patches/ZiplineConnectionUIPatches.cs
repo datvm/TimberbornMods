f98ab8b68f93116254fcadbee1db6ce8398edb69
Luke Vo <lukevo93@outlook.com>
2026-02-12 23:49:29 +0700

    T4UI 10.1.3

--- TImprove4Ui/Patches/ZiplineConnectionUIPatches.cs
+++ TImprove4Ui/Patches/ZiplineConnectionUIPatches.cs
@@ -7,8 +7,8 @@ public static class ZiplineConnectionUIPatches
     [HarmonyPostfix, HarmonyPatch(typeof(ZiplineConnectionButtonFactory), nameof(ZiplineConnectionButtonFactory.SetForConnection))]
     public static void SetButtonName(ZiplineTower otherZiplineTower, Button button)
     {
-        var name = StaticSingletonsService.EntityBadgeService?.GetEntityName(otherZiplineTower);
-        if (name is null) { return; }
+        var name = otherZiplineTower.GetComponent<NamedEntity>()?.EntityName;
+        if (string.IsNullOrEmpty(name)) { return; }
 
         ZiplineConnectionButtonFactory.SetName(button, name);
     }
--- TImprove4Ui/Services/StaticSingletonsService.cs
+++ TImprove4Ui/Services/StaticSingletonsService.cs
@@ -2,23 +2,19 @@
 
 [BindSingleton(Contexts = BindAttributeContext.NonMenu)]
 public class StaticSingletonsService(
-    EntityBadgeService entityBadgeService,
     ISceneLoader sceneLoader
 ) : ILoadableSingleton, IUnloadableSingleton
 {
 
-    public static EntityBadgeService? EntityBadgeService { get; private set; }
     public static ISceneLoader? SceneLoader { get; private set; }
 
     public void Load()
     {
-        EntityBadgeService = entityBadgeService;
         SceneLoader = sceneLoader;
     }
 
     public void Unload()
     {
-        EntityBadgeService = null;
         SceneLoader = null;
     }
 }
