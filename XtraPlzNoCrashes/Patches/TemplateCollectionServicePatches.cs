3842b9eed4ecfcc47c37665f255a263187446428
Norman Rasmussen <norman@rasmussen.co.za>
2026-03-01 22:32:25 -0800

    XPNC 10.0.1

---
+++ XtraPlzNoCrashes/Patches/TemplateCollectionServicePatches.cs
@@ -0,0 +1,39 @@
+using System.Reflection.Emit;
+
+namespace XtraPlzNoCrashes.Patches;
+
+[HarmonyPatch]
+public static class TemplateCollectionServicePatches
+{
+    [HarmonyTranspiler, HarmonyPatch(typeof(TemplateCollectionService), nameof(TemplateCollectionService.Load))]
+    public static IEnumerable<CodeInstruction> DistinctLoad(IEnumerable<CodeInstruction> instructions)
+    {
+        foreach (var ins in instructions)
+        {
+            yield return ins;
+            if (ins.opcode == OpCodes.Call && ins.operand is MethodInfo mi && mi.Name == nameof(Enumerable.SelectMany))
+            {
+                yield return new CodeInstruction(
+                    OpCodes.Call,
+                    typeof(TemplateCollectionServicePatches)
+                    .Method(nameof(DistinctAssets))
+                );
+            }
+        }
+    }
+
+    static IEnumerable<AssetRef<BlueprintAsset>> DistinctAssets(IEnumerable<AssetRef<BlueprintAsset>> assetRefs) => 
+        assetRefs.Distinct(BlueprintAssetRefEqualizer.Instance);
+
+    class BlueprintAssetRefEqualizer : IEqualityComparer<AssetRef<BlueprintAsset>>
+    {
+
+        public static readonly BlueprintAssetRefEqualizer Instance = new();
+
+        public bool Equals(AssetRef<BlueprintAsset> x, AssetRef<BlueprintAsset> y) => x.Path.ToLowerInvariant().Equals(y.Path.ToLowerInvariant());
+
+        public int GetHashCode(AssetRef<BlueprintAsset> obj) => obj.Path.ToLowerInvariant().GetHashCode();
+
+    }
+
+}
