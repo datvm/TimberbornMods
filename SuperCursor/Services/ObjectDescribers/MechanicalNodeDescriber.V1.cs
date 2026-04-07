5b642d9a7842efb34b12a8769331bcc1562fcec2
Luke Vo <lukevo93@outlook.com>
2026-04-07 17:29:32 +0700

    SC 10.0.6

--- SuperCursor/Services/ObjectDescribers/MechanicalNodeDescriber.V1.cs
+++ SuperCursor/Services/ObjectDescribers/MechanicalNodeDescriber.V1.cs
@@ -2,6 +2,7 @@
 
 public class MechanicalNodeDescriber(ILoc t, MechanicalNodeTextFormatter formatter) : BaseObjectDescriber<MechanicalNode>
 {
+    static readonly Phrase NetworkPowerPhrase = Phrase.New("Mechanical.NetworkPower").Format<int>(v => v.ToString()).FormatPower<int>();
 
     protected override void DescribeComponent(StringBuilder builder, MechanicalNode component)
     {
@@ -22,9 +23,7 @@ public class MechanicalNodeDescriber(ILoc t, MechanicalNodeTextFormatter formatt
             builder.AppendLine(formatter.FormatConsumerText(component).Indent());
         }
 
-        builder.AppendLine(t.T("Mechanical.NetworkPower", 
-            power.PowerSupply,
-            UnitFormatter.FormatPower(power.PowerDemand, t)).Indent());
+        builder.AppendLine(t.T(NetworkPowerPhrase, power.PowerSupply, power.PowerDemand).Indent());
     }
 
 }
