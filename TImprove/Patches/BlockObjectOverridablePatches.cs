4c975e4a47d0cfca99883d0427a9e45b1f83208a
Luke Vo <lukevo93@outlook.com>
2026-05-20 09:14:23 +0700

    TImprove v10.2.1

--- TImprove/Patches/BlockObjectOverridablePatches.cs
+++ TImprove/Patches/BlockObjectOverridablePatches.cs
@@ -7,10 +7,12 @@ public static class BlockObjectOverridablePatches
     [HarmonyPostfix, HarmonyPatch(nameof(BlockObject.MakeOverridable))]
     public static void AutoRemove(BlockObject __instance)
     {
-        if (MSettings.Instance?.AutoClearDeadTrees.Value == true)
-        {
-            MiscService.Instance?.Delete(__instance);
-        }
+        if ((MSettings.Instance?.AutoClearDeadTrees.Value) != true) { return; }
+
+        var comp = __instance.GetComponent<LivingNaturalResource>();
+        if (!comp || !comp.IsDead) { return; }
+
+        MiscService.Instance?.Delete(__instance);
     }
 
 }
--- TImprove/zGlobalUsings.cs
+++ TImprove/zGlobalUsings.cs
@@ -3,4 +3,3 @@ global using TImprove.Services;
 global using TImprove.Settings;
 global using TImprove.UI;
 global using MSettings = TImprove.Settings.MSettings;
-global using Priority = Timberborn.PrioritySystem.Priority;
