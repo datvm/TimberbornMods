c3803dfd520fd72175ce0710bc4cf85acf8cacc9
Luke Vo <lukevo93@outlook.com>
2026-09-15 19:05:03 +0700

    TP v11.0.1

--- TimberPipes/MConfigs.cs
+++ TimberPipes/MConfigs.cs
@@ -2,7 +2,7 @@
 
 public class MConfigs : BaseModdableTimberbornAttributeConfiguration
 {
-    public override ConfigurationContext AvailableContexts => ConfigurationContext.Game;
+    public override ConfigurationContext AvailableContexts => ConfigurationContext.Game | ConfigurationContext.MainMenu;
 
     protected override void ConfigureRegistry(ModdableTimberbornRegistry registry)
         => registry.UseBuildingSettings();
@@ -10,6 +10,8 @@ public class MConfigs : BaseModdableTimberbornAttributeConfiguration
     public override void Configure(Configurator configurator, ConfigurationContext context)
     {
         base.Configure(configurator, context);
+
+        if (context != ConfigurationContext.Game) { return; }
 
         configurator.MultiBind<EntityPanelModule>().ToProvider<DebugFragmentProvider>().AsSingleton();
 
---
+++ TimberPipes/Services/ModUpdateNotifier.cs
@@ -0,0 +1,10 @@
+﻿namespace TimberPipes.Services;
+
+[BindModUpdateNotifier]
+public class ModUpdateNotifier : IModUpdateNotifier2
+{
+    public string ModId => nameof(TimberPipes);
+    public string Version => "11.0.1";
+    public int VersionNumber => 110010;
+    public string MessageLocKey => "";
+}
